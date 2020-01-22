14863c97407ec7dea621bc2467453aa84728b58c
rhedgeco <rhedgeco@gmail.com>
2020-01-22 03:19:21 -0800

    better gallery page

--- frontend/data/js/pages/gallery-handler.js
+++ frontend/data/js/pages/gallery-handler.js
@@ -1,147 +1,63 @@
-const gHolder = document.getElementById("gallery-holder");
-const imageCount = 100;
-
-function progressive_replace() {
-    // progressive-image.js
-// by Craig Buckler, @craigbuckler
-    if (window.addEventListener && window.requestAnimationFrame && document.getElementsByClassName) window.addEventListener('load', function () {
-
-        'use strict';
-
-        // start
-        var pItem = document.getElementsByClassName('progressive replace'), pCount, timer;
-
-        // scroll and resize events
-        window.addEventListener('scroll', scroller, false);
-        window.addEventListener('resize', scroller, false);
-
-        // DOM mutation observer
-        if (MutationObserver) {
-
-            var observer = new MutationObserver(function () {
-                if (pItem.length !== pCount) inView();
-            });
-            observer.observe(document.body, {subtree: true, childList: true, attributes: true, characterData: true});
-
-        }
-
-        // initial check
-        inView();
-
-
-        // throttled scroll/resize
-        function scroller() {
-
-            timer = timer || setTimeout(function () {
-                timer = null;
-                inView();
-            }, 300);
-
-        }
-
-
-        // image in view?
-        function inView() {
-
-            if (pItem.length) requestAnimationFrame(function () {
-
-                var wH = window.innerHeight, cRect, cT, cH, p = 0;
-                while (p < pItem.length) {
-
-                    cRect = pItem[p].getBoundingClientRect();
-                    cT = cRect.top;
-                    cH = cRect.height;
-
-                    if (0 < cT + cH && wH > cT) {
-                        loadFullImage(pItem[p]);
-                        pItem[p].classList.remove('replace');
-                    } else p++;
-
-                }
-
-                pCount = pItem.length;
-
-            });
-
-        }
-
-
-        // replace with full image
-        function loadFullImage(item, retry) {
-
-            var href = item && (item.getAttribute('data-href') || item.href);
-            if (!href) return;
-
-            // load image
-            var img = new Image(), ds = item.dataset;
-            if (ds) {
-                if (ds.srcset) img.srcset = ds.srcset;
-                if (ds.sizes) img.sizes = ds.sizes;
-            }
-            img.onload = addImg;
-            retry = 1 + (retry || 0);
-            if (retry < 3) img.onerror = function () {
-                setTimeout(function () {
-                    loadFullImage(item, retry);
-                }, retry * 3000);
-            };
-            img.className = 'reveal';
-            img.src = href;
-
-            // replace image
-            function addImg() {
-
-                requestAnimationFrame(function () {
-
-                    // disable click
-                    if (href === item.href) {
-                        item.style.cursor = 'default';
-                        item.addEventListener('click', function (e) {
-                            e.preventDefault();
-                        }, false);
-                    }
-
-                    // preview image
-                    var pImg = item.querySelector && item.querySelector('img.preview');
-
-                    // add full image
-                    item.insertBefore(img, pImg && pImg.nextSibling).addEventListener('animationend', function () {
-
-                        // remove preview image
-                        if (pImg) {
-                            if (pImg.alt) img.alt = pImg.alt;
-                            item.removeChild(pImg);
-                        }
-
-                        img.classList.remove('reveal');
-
-                    });
-
-                });
-
-            }
-
-        }
-
-    }, false);
-}
-
-function pad_zeroes(string) {
-    let zeroes = '000';
+function left_pad(string) {
+    let zeroes = "000";
     return zeroes.substring(0, zeroes.length - string.length) + string;
 }
 
-function load_images() {
-    let client = new XMLHttpRequest();
-    client.open('GET', 'data/images/gallery/html/image.html');
-    client.onload = function () {
-        for (let i = 1; i <= imageCount; i++) {
-            gHolder.innerHTML += client.responseText
-                .replace('[full-img]', 'data/images/gallery/images/' + pad_zeroes('' + i) + 'rgengage.jpg');
+function populate_gallery(_callback) {
+    let imageCount = 50;
+    let req = new XMLHttpRequest();
+    req.open('GET','data/images/gallery/html/image.html');
+    req.onload = function () {
+        let gallery = document.getElementById('gallery');
+        gallery.innerHTML = "";
+        for (let i = 1; i <= imageCount; i++)
+        {
+            gallery.innerHTML += req.responseText.replace(
+                '[full-img]',
+                'data/images/gallery/images/full/'+left_pad(''+i)+'rgengage.jpg'
+            );
         }
-        progressive_replace();
+        _callback();
     };
-    client.send();
+    req.send();
 }
 
-load_images();
+function control_gallery() {
+    var gallery = document.querySelector('#gallery');
+    var getVal = function (elem, style) {
+        return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+    };
+    var getHeight = function (item) {
+        return item.querySelector('.content').getBoundingClientRect().height;
+    };
+    var resizeAll = function () {
+        var altura = getVal(gallery, 'grid-auto-rows');
+        var gap = getVal(gallery, 'grid-row-gap');
+        gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+            var el = item;
+            el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+        });
+    };
+    gallery.querySelectorAll('img').forEach(function (item) {
+        item.classList.add('byebye');
+        if (item.complete) {
+            console.log(item.src);
+        } else {
+            item.addEventListener('load', function () {
+                var altura = getVal(gallery, 'grid-auto-rows');
+                var gap = getVal(gallery, 'grid-row-gap');
+                var gitem = item.parentElement.parentElement;
+                gitem.style.gridRowEnd = "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+                item.classList.remove('byebye');
+            });
+        }
+    });
+    window.addEventListener('resize', resizeAll);
+    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
+        item.addEventListener('click', function () {
+            item.classList.toggle('full');
+        });
+    });
+}
+
+populate_gallery(control_gallery);
