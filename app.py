553ae01845b26c87de13740901045e867ff37487
huge_hedgie <ryan.hedgecock@dilililabs.com>
2020-01-22 16:16:47 -0800

    added tiny thumbnails for gallery

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = WebApp(frontend_dir='frontend', page_404='page404.html')
 gallery = GalleryHtmlImages('frontend/data/images/gallery')
 app.add_route('gallery_html_images', gallery)
 
-app.launch_webserver()
+app.launch_webserver(port=6009)
