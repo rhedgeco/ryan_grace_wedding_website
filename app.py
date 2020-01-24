ec396dab63549981e791d640361967be74fe6709
huge_hedgie <ryan.hedgecock@dilililabs.com>
2020-01-24 13:57:36 -0800

    Put my foot down and stopped this horrendous port changing nightmare for good

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from pathlib import Path
 
 from general_falcon_webserver.backend.general_manager.databases import SqliteDatabase
@@ -8,22 +9,37 @@ from backend.authenticator import Auth
 from backend.gallery_info import GalleryInfo
 from backend.gallery_resource import GalleryHtmlImages
 
-app = WebApp(frontend_dir='frontend', page_404='page404.html')
 
-with open(Path('backend') / 'database_setup.sql') as file:
-    db_config = file.read()
-db = SqliteDatabase('wedding_db', db_config)
+def parse_args():
+    parser = ArgumentParser(description="Website for Grace and Ryan's Wedding!")
+    parser.add_argument("--port", type=int, default=80, help="Port to host server.")
+    parsed_args = parser.parse_args()
+    return parsed_args
 
-gallery = GalleryHtmlImages('frontend/data/images/gallery')
-app.add_route('gallery_html_images', gallery)
 
-gallery_info = GalleryInfo('frontend/data/images/gallery')
-app.add_route('gallery_info', gallery_info)
+def configure_app():
+    web_app = WebApp(frontend_dir='frontend', page_404='page404.html')
 
-auth = Auth(db)
-app.add_route('auth', auth)
+    with open(Path('backend') / 'database_setup.sql') as file:
+        db_config = file.read()
+    db = SqliteDatabase('wedding_db', db_config)
 
-admins = Admins(db)
-app.add_route('admins', admins)
+    gallery = GalleryHtmlImages('frontend/data/images/gallery')
+    web_app.add_route('gallery_html_images', gallery)
 
-app.launch_webserver()
+    gallery_info = GalleryInfo('frontend/data/images/gallery')
+    web_app.add_route('gallery_info', gallery_info)
+
+    auth = Auth(db)
+    web_app.add_route('auth', auth)
+
+    admins = Admins(db)
+    web_app.add_route('admins', admins)
+
+    return web_app
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    app = configure_app()
+    app.launch_webserver(port=args.port)
