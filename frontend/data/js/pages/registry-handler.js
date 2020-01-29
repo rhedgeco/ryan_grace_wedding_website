d81760bd3445ff81e232bfc21f39e8804c769462
rhedgeco <rhedgeco@gmail.com>
2020-01-29 11:11:58 -0800

    fixed padding bug and provided better error handling from myregistry

--- frontend/data/js/pages/registry-handler.js
+++ frontend/data/js/pages/registry-handler.js
@@ -2,12 +2,19 @@ function populate_registry() {
     let req = new XMLHttpRequest();
     req.open('GET', 'api/myregistry_scraper');
     req.onload = function () {
+        $('#registry-message').remove();
+        $('#registry-preloader').remove();
+        let $holder = $('#registry-holder');
         if (req.status === 200) {
-            $('#registry-message').remove();
-            $('#registry-preloader').remove();
-            let $holder = $('#registry-holder');
             $holder.append(req.responseText);
-        } else (M.toast({html: 'Error retrieving registry.'}))
+        } else {
+            $holder.after('<br>ERROR<br>' +
+                'There was a problem populating registry data<br>' +
+                'from MyRegistry servers.<br>' +
+                'Try to accessing it directly ' +
+                '<a href="https://www.myregistry.com/wedding-registry/ryan-hedgecock-and-grace-perlman-campbell-ca/2280168">here</a>.');
+            $holder.remove();
+        }
     };
     req.send();
 }
