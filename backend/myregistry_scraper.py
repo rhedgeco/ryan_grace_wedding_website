44adc30f0efcdec2fb044e8217c5e4075e423fe8
rhedgeco <rhedgeco@gmail.com>
2020-01-29 09:10:10 -0800

    fixed bought already link on registry item

--- backend/myregistry_scraper.py
+++ backend/myregistry_scraper.py
@@ -28,12 +28,14 @@ class MyRegistryScraper:
                 .replace('[gift-image]', item['image'])\
                 .replace('[gift-desired]', item['desire'])\
                 .replace('[gift-purchased]', item['purchase'])\
+                .replace('[gift-bought]', item['bought'])\
                 .replace('[gift-link]', item['link'])
 
         resp.body = registry_html
 
 def scrape_registry_page_gifts():
     MYREGISTRY_GIFT_LINK = f'https://www.myregistry.com/Visitors/Giftlist/PurchaseAssistant.aspx?registryId={MYREGISTRY_PAGE_ID}&giftId='
+    MYREGISTRY_GIFT_PURCHASED = f'https://www.myregistry.com/Visitors/GiftList/PurchaseProcess.aspx?giftId='
     page = requests.get(
         f'https://www.myregistry.com/ExternalApps/EmbededVistorView/v2/Visitors/GiftList.aspx?registryId={MYREGISTRY_PAGE_ID}&pageSize=10000')
     tree = html.fromstring(page.content)
@@ -51,6 +53,7 @@ def scrape_registry_page_gifts():
                 'price': str(gift_prices[i]).replace('\r\n', '').replace('  ', ''),
                 'image': gift_images[i],
                 'link': MYREGISTRY_GIFT_LINK + gift_ids[i],
+                'bought': MYREGISTRY_GIFT_PURCHASED + gift_ids[i],
                 'desire': str(gift_desire[i]).replace('\r\n', '').replace('  ', ''),
                 'purchase': str(gift_purcha[i]).replace('\r\n', '').replace('  ', '')
             })
